6dfbd2a458257a7b385fd7a8c0a044b7817856a4
ahspw <ahspw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-13 04:06:02 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/jetpack-omnisearch/tags/2.5@803518 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ omnisearch.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Plugin Name: Jetpack Omnisearch
+ * Plugin URI: 
+ * Description: A single search box, that lets you search many different things.
+ * Author: Anas H. Sulaiman
+ * Version: 2.5
+ * Author URI: http://ahs.pw/
+ * Text Domain: jetpack-omnisearch
+ * Domain Path: /languages/
+ * License: GPL2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * Module Name: Omnisearch
+ * Module Description: A single search box, that lets you search many different things.
+ * Sort Order: 8
+ * First Introduced: 2.3
+ * Requires Connection: No
+ * Auto Activate: Yes
+ */
+
+// Only do Jetpack Omnisearch if there isn't already a Core WP_Omnisearch Class.
+if ( ! class_exists( 'WP_Omnisearch' ) )
+	require_once( dirname( __FILE__ ) . '/omnisearch/omnisearch-core.php' );
+
+add_action( 'plugins_loaded', 'omnisearch_load_textdomain' ); // Edited by Anas H. Sulaiman
+function omnisearch_load_textdomain() {
+	load_plugin_textdomain( 'jetpack-omnisearch', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+} // Edited by Anas H. Sulaiman
+
+/*
+* Edits are denoted by the comment: Edited by Anas H. Sulaiman.
+* Other edits are listed here:
+* Edit 1: 
+*/
