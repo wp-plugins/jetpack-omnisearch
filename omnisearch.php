c61ee57f558d9161961a0dfebcfe68c6cb24e4a7
wpjp <wpjp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-06 07:36:23 +0000

    update to 3.2.1
    git-svn-id: https://plugins.svn.wordpress.org/jetpack-omnisearch/tags/3.2.1@1039296 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ omnisearch.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * Plugin Name: JP Omnisearch
+ * Plugin URI: http://wordpress.org/plugins/jetpack-omnisearch/
+ * Description: Search your entire database from a single field in your Dashboard.
+ * Author: JP Bot
+ * Version: 3.2.1
+ * Text Domain: jetpack
+ * Domain Path: /languages/
+ * License: GPL2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * Module Name: Omnisearch
+ * Module Description: Search your entire database from a single field in your Dashboard.
+ * Sort Order: 16
+ * First Introduced: 2.3
+ * Requires Connection: No
+ * Auto Activate: Yes
+ * Module Tags: Developers
+ */
+
+// Only do Jetpack Omnisearch if there isn't already a Core WP_Omnisearch Class.
+if ( ! class_exists( 'WP_Omnisearch' ) ) {
+	require_once( dirname( __FILE__ ) . '/omnisearch/omnisearch-core.php' );
+}
+
+function jetpack_omnisearch_load_textdomain() {
+	load_plugin_textdomain( 'jetpack', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'jetpack_omnisearch_load_textdomain' );
+
+function jetpack_omnisearch_link($actions) {
+	return array_merge(
+		array( 'settings' => sprintf( '<a href="%s">%s</a>', 'admin.php?page=omnisearch', __( 'Search', 'jetpack' ) ) ),
+		$actions
+	);
+	return $actions;
+}
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'jetpack_omnisearch_link' );
